12a9ed9e48466d28c59cb27840f7b5a46b8dc0b4
BartKK <Bartkukulka@gmail.com>
2017-03-26 15:27:32 +0200

    modify package json and make server listen on port

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Siema");
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, function () {
+  console.log('Server is up and running on port 3000');
+});
