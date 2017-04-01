dd6994e00776816201bba57d3fa8d558752ffe9c
BartKK <Bartkukulka@gmail.com>
2017-04-01 14:38:52 +0200

    Add delete button

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 const update = document.getElementById('switch');
+const deleteEntry = document.getElementById('delete');
 
 update.addEventListener('click', () => {
   fetch('titles', {
@@ -15,4 +16,20 @@ update.addEventListener('click', () => {
     .catch(err =>{
       console.log(err);
     })
+});
+
+
+deleteEntry.addEventListener('click', () => {
+  fetch('titles', {
+    method: 'delete',
+    headers: {'Content-type': 'application/json'},
+    body: JSON.stringify({
+      title: 'Title has been changed!!!'
+
+    })
+  }).then(res => {
+    window.location.reload();
+  }).catch(err =>{
+    console.error(err)
+  })
 });
--- script.js
+++ script.js
@@ -64,3 +64,16 @@ app.put('/titles', (req, res) => {
       res.send(response);
   })
 });
+
+
+app.delete('/titles', (req, res) =>{
+  db.collection('coderspro').findOneAndDelete({
+    title: req.body.title
+  }, (err, result) => {
+    if (err) {
+      return res.send(err)
+    }
+    res.send(result);
+
+  })
+})
