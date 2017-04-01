5a7ff7e922cb5e3acc2e78018baf483f9a99cc72
BartKK <Bartkukulka@gmail.com>
2017-04-01 09:45:41 +0200

    Add mongodb

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
-
 const port = 3000;
+let db;
 
+app.use(bodyParser.urlencoded({encode: true, extended: true}));
 
-
-app.listen(port, () => {
-  console.log(`Server is up and running on port ${port}`);
+MongoClient.connect('mongodb://BartKK1:klopik12@ds147510.mlab.com:47510/coderspro', (err, res) => {
+  if(err) {
+    return console.error(err);
+  }
+  db = res;
+  app.listen(port, () => {
+    console.log(`Server is up and running on port ${port}`);
+  });
 });
-
 
 app.get('/', (req, res) => {
 console.log(__dirname);
